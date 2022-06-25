920634e52622b83114c1dd885ded35f0aa1733d2
javad101 <javad.amindehgha1@gmail.com>
2022-06-25 07:03:48 -0700

    Updates

---
+++ precache-manifest.ec19824b493c2d355fe5c2c020eb0305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f5055592e16678e1a07c80adc5cb57",
+    "url": "/football-slider/index.html"
+  },
+  {
+    "revision": "aa7eacdcac7c55898ded",
+    "url": "/football-slider/static/css/main.9648fb74.chunk.css"
+  },
+  {
+    "revision": "4acce97ee23a5c46634a",
+    "url": "/football-slider/static/js/2.6e9d5766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/football-slider/static/js/2.6e9d5766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7eacdcac7c55898ded",
+    "url": "/football-slider/static/js/main.e5994828.chunk.js"
+  },
+  {
+    "revision": "7a9e66723f2ea8a7d1e0",
+    "url": "/football-slider/static/js/runtime-main.a683129b.js"
+  }
+]);
