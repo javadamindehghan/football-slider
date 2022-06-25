1cd28945b033251cf1387368d0223ad8cb3bf3a2
javad101 <javad.amindehgha1@gmail.com>
2022-06-25 07:05:00 -0700

    football-slider

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://img.olympicchannel.com/images/image/private/t_1-1_600/f_auto/v1538355600/primary/wfrhxc0kh2vvq77sonki',
+    name: 'lionel messi',
+    title: 'beast player',
+    quote:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type',
+  },
+  {
+    id: 2,
+    image:
+      'https://e3.365dm.com/22/04/768x432/skysports-cristiano-ronaldo_5743998.jpg?20220419115805',
+    name: 'cristiano ronaldo',
+    title: 'top player',
+    quote:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type',
+  },
+  {
+    id: 3,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/74/Iran_-_Japan%2C_AFC_Asian_Cup_2019_42_%28cropped%29.jpg',
+    name: 'peter smith',
+    title: 'iranian player',
+    quote:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type',
+  },
+  
+];
+
+export default people;
